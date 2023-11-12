c0bf1fa593807314184d7d3974a3ae1943eb1f4d
siriusalpha7858 <siriusalpha7858@localhost>
2023-11-11 16:25:59 -0800

    test upload from

--- src/com/jdlc/asteroids/SpaceStation.java
+++ src/com/jdlc/asteroids/SpaceStation.java
@@ -6,12 +6,35 @@ public class SpaceStation extends FixedGameObject{
 	private int blinkRate;
 	private boolean lightOn;
 	
+	// Initializes Space Station with a unique ID, random blink rate from 1-4 game ticks,
+	// boolean that determines if the Space Station light is on, and a teal color rgb code.
 	public SpaceStation() {
 		id = getfId();
-		blinkRate = random.nextInt(9) + 1;
+		blinkRate = random.nextInt(3) + 1;
 		lightOn = true;
-		setColor(0, 255, 0);
+		setColor(64, 224, 208);
 	}
 	
+	// This method works with GameWorld's tick method; the game's "clock". The Space Station
+	// will blink every game tick, or every 2nd, 3rd, or 4th tick.
+	public void blink(int currTick) {
+		if (currTick % blinkRate == 0) {
+			lightOn ^= true;
+		}
+	}
 	
+	// Returns Space Station's blink rate
+	public int getRate() {
+		return blinkRate;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String toString() {
+		String parentString = super.toString();
+		String thisString = "Station: " + id + " blink rate = " + blinkRate;
+		return parentString + thisString;
+	}
 }
